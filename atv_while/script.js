985d91ce520a44ef73f596a7979186ea79ecbc1d
Rafa <rafaelacristina1510.oliveira@gmail.com>
2026-02-27 13:50:07 -0300

    Add files via upload

---
+++ atv_while/script.js
@@ -0,0 +1,28 @@
+function gerarTabuada() {
+
+  let resultadoFinal = "";
+  
+  let numero = 1;
+
+
+  while (numero <= 10) {
+    resultadoFinal += `<div class='card'>`;
+    resultadoFinal += `<h3>Tabuada do ${numero}</h3>`;
+
+    let multiplicador = 1;
+
+    while (multiplicador <= 10) {
+      resultadoFinal += `${numero} x ${multiplicador} = ${numero * multiplicador} <br>`;
+
+      multiplicador++;
+    }
+
+    resultadoFinal += `</div>`;
+
+    numero++;
+  }
+
+  document.getElementById("resultado").innerHTML = resultadoFinal;
+
+}
+
